cad8ce9bc38f4074f0dc4fae45ef2e821be76f0d
Nathan Weizenbaum <nex342@gmail.com>
2007-09-24 23:40:15 +0000

    Specs for Builder#actions.
    git-svn-id: http://svn.hamptoncatlin.com/make_resourceful/trunk@105 c18eca5a-f828-0410-9317-b2e082e89db6

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -9,7 +9,7 @@ describe Resourceful::Builder, " applied without any modification" do
 
   it "should remove all resourceful actions" do
     @controller.expects(:send).with do |name, action_module|
-      name == :include && (action_module.instance_methods & Resourceful::ACTIONS).empty?
+      name == :include && (action_module.instance_methods & Resourceful::ACTIONS.map(&:to_s)).empty?
     end
     @builder.apply
   end
@@ -38,6 +38,65 @@ describe Resourceful::Builder, " applied without any modification" do
     yielded = stub
     @controller.expects(:before_filter).yields(yielded)
     yielded.expects(:send).with(:load_parent_objects)
+    @builder.apply
+  end
+end
+
+describe Resourceful::Builder, "with some actions set" do
+  include ControllerMocks
+  before :each do
+    mock_controller
+    @builder = Resourceful::Builder.new(@controller)
+    @actions = [:show, :index, :new, :create]
+    @builder.actions *@actions
+  end
+
+  it "should include the given actions" do
+    @controller.expects(:send).with do |name, action_module|
+      name == :include && (action_module.instance_methods & Resourceful::ACTIONS.map(&:to_s)).sort ==
+        @actions.map(&:to_s).sort
+    end
+    @builder.apply
+  end
+
+  it "should un-hide the given actions" do
+    @builder.apply
+    (@controller.hidden_actions & @actions).should be_empty
+  end
+end
+
+describe Resourceful::Builder, "with all actions set for a plural controller" do
+  include ControllerMocks
+  before :each do
+    mock_controller
+    @controller.class_eval { def plural?; true; end }
+    @builder = Resourceful::Builder.new(@controller)
+    @builder.actions :all
+  end
+
+  it "should include all actions" do
+    @controller.expects(:send).with do |name, action_module|
+      name == :include && (action_module.instance_methods & Resourceful::ACTIONS.map(&:to_s)).sort ==
+        Resourceful::ACTIONS.map(&:to_s).sort
+    end
+    @builder.apply
+  end
+end
+
+describe Resourceful::Builder, "with all actions set for a singular controller" do
+  include ControllerMocks
+  before :each do
+    mock_controller
+    @controller.class_eval { def plural?; false; end }
+    @builder = Resourceful::Builder.new(@controller)
+    @builder.actions :all
+  end
+
+  it "should include all singular actions" do
+    @controller.expects(:send).with do |name, action_module|
+      name == :include && (action_module.instance_methods & Resourceful::ACTIONS.map(&:to_s)).sort ==
+        Resourceful::SINGULAR_ACTIONS.map(&:to_s).sort
+    end
     @builder.apply
   end
 end
