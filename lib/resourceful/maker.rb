66bafa73821f39d2b033980093fa4e20a4c23e20
Nathan Weizenbaum <nex342@gmail.com>
2007-11-01 20:22:53 +0000

    Docs for Resourceful::Maker. Also changing the arguments for make_resourceful... we don't use *args, so just make it options = {}.
    git-svn-id: http://svn.hamptoncatlin.com/make_resourceful/trunk@135 c18eca5a-f828-0410-9317-b2e082e89db6

--- lib/resourceful/maker.rb
+++ lib/resourceful/maker.rb
@@ -2,17 +2,53 @@ require 'resourceful/builder'
 require 'resourceful/base'
 
 module Resourceful
+  # This module is extended by the ActionController::Base class object.
+  # It provides the actual +make_resourceful+ method
+  # and sets up the controller so that everything will work.
   module Maker
+    # Called automatically on ActionController::Base.
+    # Initializes various inheritable attributes.
     def self.extended(base)
       base.write_inheritable_attribute :resourceful_callbacks, {}
       base.write_inheritable_attribute :resourceful_responses, {}
       base.write_inheritable_attribute :parents,               []
     end
 
-    def make_resourceful(*args, &block)
-      options = Hash === args.last ? args.pop : {}
-
+    # :call-seq:
+    #   make_resourceful(options = {}) { ... }
+    #
+    # This is the central method, and namesake, of make_resourceful.
+    # It takes a block and evaluates it in the context of a Builder,
+    # allowing the controller to be customized extensively.
+    #
+    # See Resourceful::Builder for documentation on the methods available
+    # in the context of the block.
+    #
+    # The only option currently available is <tt>:include</tt>.
+    # It takes an object that responds to to_proc
+    # (or an array of such objects)
+    # and evaluates that proc in the same context as the block.
+    # For example:
+    #
+    #   make_resourceful :include => proc { actions :all } do
+    #     before :show do
+    #       current_object.current_user = current_user
+    #     end
+    #   end
+    #
+    # This is the same as:
+    #
+    #   make_resourceful do
+    #     actions :all
+    #     before :show do
+    #       current_object.current_user = current_user
+    #     end
+    #   end
+    # 
+    def make_resourceful(options = {}, &block)
+      # :stopdoc:
       include Resourceful::Base
+      # :startdoc:
 
       builder = Resourceful::Builder.new(self)
       (Resourceful::Base.made_resourceful + Array(options[:include])).each { |proc| builder.instance_eval(&proc) }
