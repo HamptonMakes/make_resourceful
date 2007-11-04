4a671485ab5335d88d17377269639cd8e0f6f69e
Nathan Weizenbaum <nex342@gmail.com>
2007-11-04 09:38:04 +0000

    Various improvements to the scaffold generator.
    git-svn-id: http://svn.hamptoncatlin.com/make_resourceful/trunk@150 c18eca5a-f828-0410-9317-b2e082e89db6

--- generators/resourceful_scaffold/resourceful_scaffold_generator.rb
+++ generators/resourceful_scaffold/resourceful_scaffold_generator.rb
@@ -1,12 +1,10 @@
 class ResourcefulScaffoldGenerator < Rails::Generator::NamedBase
-  attr_reader   :controller_name,
-                :controller_class_path,
+  attr_reader   :controller_class_path,
                 :controller_file_path,
                 :controller_class_nesting,
                 :controller_class_nesting_depth,
                 :controller_class_name,
                 :controller_underscore_name,
-                :controller_singular_name,
                 :controller_plural_name
   alias_method  :controller_file_name,  :controller_underscore_name
   alias_method  :controller_table_name, :controller_plural_name
@@ -14,11 +12,9 @@ class ResourcefulScaffoldGenerator < Rails::Generator::NamedBase
   def initialize(runtime_args, runtime_options = {})
     super
 
-    @controller_name = @name.pluralize
-
-    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_name)
+    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@name.pluralize)
     @controller_class_name_without_nesting, @controller_underscore_name, @controller_plural_name = inflect_names(base_name)
-    @controller_singular_name=base_name.singularize
+
     if @controller_class_nesting.empty?
       @controller_class_name = @controller_class_name_without_nesting
     else
@@ -28,7 +24,7 @@ class ResourcefulScaffoldGenerator < Rails::Generator::NamedBase
   
   def manifest
     record do |m|
-# Check for class naming collisions.
+      # Check for class naming collisions.
       m.class_collisions(controller_class_path, "#{controller_class_name}Controller", "#{controller_class_name}Helper")
       m.class_collisions(class_path, "#{class_name}")
 
@@ -39,43 +35,53 @@ class ResourcefulScaffoldGenerator < Rails::Generator::NamedBase
       m.directory(File.join('app/views', controller_class_path, controller_file_name))
       m.directory(File.join('test/functional', controller_class_path))
       m.directory(File.join('test/unit', class_path))
+      m.directory(File.join('test/fixtures', class_path))
 
+      # Views
       for action in scaffold_views
-        m.template(
-          "view_#{action}.haml",
-          File.join('app/views', controller_class_path, controller_file_name, "#{action}.haml")
-        )
+        m.template("view_#{action}.haml", File.join('app/views', controller_class_path, controller_file_name, "#{action}.haml"))
+      end
+      m.template('view_partial.haml', File.join('app/views', controller_class_path, controller_file_name, "_#{singular_name}.haml"))
+
+      # Helper
+      m.template('helper.rb', File.join('app/helpers', controller_class_path, "#{controller_file_name}_helper.rb"))
+
+      # Model
+      m.template('model.rb', File.join('app/models', class_path, "#{file_name}.rb"))
+
+      unless options[:skip_migration]
+        m.migration_template('migration.rb', 'db/migrate', 
+          :assigns => {
+            :migration_name => "Create#{class_name.pluralize.gsub(/::/, '')}",
+            :attributes     => attributes
+          },
+          :migration_file_name => "create_#{file_path.gsub(/\//, '_').pluralize}")
       end
 
-      m.template('view_form.haml',
-          File.join('app/views', controller_class_path, controller_file_name, "_form.haml")
-        )
-      
-      m.template('partial.haml', File.join('app/views', controller_class_path, controller_file_name, "_#{singular_name}.haml"))
+      # Controller
+      m.template('controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb"))      
 
-      # Layout and stylesheet.
-      m.template('layout.haml', File.join('app/views/layouts', controller_class_path, "application.haml"))
+      # Tests
+      m.template('functional_test.rb', File.join('test/functional', controller_class_path, "#{controller_file_name}_controller_test.rb"))
+      m.template('unit_test.rb',       File.join('test/unit', class_path, "#{file_name}_test.rb"))
+      m.template('fixtures.yml',       File.join('test/fixtures', "#{table_name}.yml"))
 
-      #m.dependency 'model', [singular_name] + @args, :collision => :skip 
-
-      m.template(
-        'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
-      )
+      # Route
       m.route_resources controller_file_name
     end
   end
 
   protected
-    # Override with your own usage banner.
-    def banner
-      "Usage: #{$0} scaffold ModelName [field:type, field:type]"
-    end
+  
+  def banner
+    "Usage: #{$0} resourcefulscaffold ModelName [field:type, field:type]"
+  end
 
-    def scaffold_views
-      %w[ index show new edit ]
-    end
+  def scaffold_views
+    %w[ index show new edit _form ]
+  end
 
-    def model_name 
-      class_name.demodulize
-    end
+  def model_name 
+    class_name.demodulize
+  end
 end
