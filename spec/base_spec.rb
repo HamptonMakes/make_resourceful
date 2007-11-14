3f81e7185bafd1aea5e63e38dd746047f547fb66
Nathan Weizenbaum <nex342@gmail.com>
2007-11-14 07:09:08 +0000

    Fix a nasty test bug that caused the integration tests to fail under ill-defined circumstances.
    git-svn-id: http://svn.hamptoncatlin.com/make_resourceful/trunk@160 c18eca5a-f828-0410-9317-b2e082e89db6

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,7 +1,9 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Resourceful::Base, ".made_resourceful" do
+  before(:all)  { @original_blocks = Resourceful::Base.made_resourceful.dup }
   before(:each) { Resourceful::Base.made_resourceful.replace [] }
+  after(:all)   { Resourceful::Base.made_resourceful.replace @original_blocks }
 
   it "should store blocks when called with blocks and return them when called without a block" do
     5.times { Resourceful::Base.made_resourceful(&should_be_called) }
