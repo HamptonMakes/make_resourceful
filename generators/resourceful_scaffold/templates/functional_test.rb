60e6d850b7b50925143e58189776afd58bfd5113
Hampton Catlin <hcatlin@gmail.com>
2009-11-23 11:02:04 +0000

    Making the generated tests pass for resource_scaffold

--- generators/resourceful_scaffold/templates/functional_test.rb
+++ generators/resourceful_scaffold/templates/functional_test.rb
@@ -4,14 +4,7 @@ require '<%= controller_file_path %>_controller'
 # Re-raise errors caught by the controller.
 class <%= controller_class_name %>Controller; def rescue_action(e) raise e end; end
 
-class <%= controller_class_name %>ControllerTest < Test::Unit::TestCase
-  fixtures :<%= table_name %>
-
-  def setup
-    @controller = <%= controller_class_name %>Controller.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
-  end
+class <%= controller_class_name %>ControllerTest < ActionController::TestCase
 
   def test_should_get_index
     get :index
