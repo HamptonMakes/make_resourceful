cc545f52a63c415f0d0ffc173b43ed36f7197178
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2012-02-23 11:44:55 -0800

    turn off generators completely

--- lib/make_resourceful.rb
+++ lib/make_resourceful.rb
@@ -4,8 +4,4 @@ class MakeResourcefulTie < Rails::Railtie
     require_relative 'resourceful/maker'
     ActionController::Base.extend Resourceful::Maker
   end
-  
-  generators do
-    #require_relative 'resourceful/generators/resourceful_scaffold/resourceful_scaffold_generator'
-  end
 end
